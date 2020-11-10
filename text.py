e140df9d4ee9bd4c1bb92ee9e1c7e97da3070063
owspz01 <d08405049@ems.npu.edu.tw>
2020-11-10 17:13:28 +0800

    first commit

---
+++ text.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+url = "https://age-of-empires-2-api.herokuapp.com/api/v1/unit/1"
+
+payload  = {}
+headers= {}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
+
